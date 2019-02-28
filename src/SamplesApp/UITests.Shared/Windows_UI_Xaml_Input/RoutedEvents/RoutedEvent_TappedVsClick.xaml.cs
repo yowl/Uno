52e035b0e2fbac0305ee4aa81bf50739fd60af19
Carl de Billy <carl.debilly@nventive.com>
2019-02-28 14:26:15 -0500

    Added samples for testing transforms on ScrollViewer + ListView

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Input/RoutedEvents/RoutedEvent_TappedVsClick.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Shared.Windows_UI_Xaml_Input.RoutedEvents
+{
+	[SampleControlInfo("Routed Events", "Tapped Vs Click")]
+	public sealed partial class RoutedEvent_TappedVsClick : Page
+	{
+		public RoutedEvent_TappedVsClick()
+		{
+			this.InitializeComponent();
+
+			AddHandler(TappedEvent, new TappedEventHandler(RootHandler), handledEventsToo: true);
+		}
+
+		private void RootHandler(object sender, TappedRoutedEventArgs e)
+		{
+			txtRoot.Text += "TAPPED (root) - handledEventsToo: true\n";
+		}
+
+		protected override void OnTapped(TappedRoutedEventArgs e)
+		{
+			base.OnTapped(e);
+
+			txtRoot.Text += "TAPPED (root) - override, should not happen when tapped on children element\n";
+		}
+	}
+}
---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Input/RoutedEvents/RoutedEvent_TappedVsClickControl.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace UITests.Shared.Windows_UI_Xaml_Input.RoutedEvents
+{
+	public sealed partial class RoutedEvent_TappedVsClickControl : UserControl
+	{
+		public RoutedEvent_TappedVsClickControl()
+		{
+			this.InitializeComponent();
+
+			Tapped += (snd, evt) =>
+			{
+				txtChildren.Text += "(2/2) Tapped (event handler) + handled=true\n";
+
+				evt.Handled = true;
+			};
+
+			btnOuter.Tapped += (snd, evt) =>
+			{
+				txtChildren.Text += $"Outer TAPPED\n";
+			};
+			btnOuter.Click += (snd, evt) =>
+			{
+				txtChildren.Text += $"Outer CLICK\n";
+			};
+			btnInner.Tapped += (snd, evt) =>
+			{
+				txtChildren.Text += $"Inner TAPPED\n";
+			};
+			btnInner.Click += (snd, evt) =>
+			{
+				txtChildren.Text += $"Inner CLICK\n";
+			};
+
+			outerContent.Tapped += (snd, evt) =>
+			{
+				txtChildren.Text += $"Outer content TAPPED\n";
+			};
+		}
+
+		protected override void OnTapped(TappedRoutedEventArgs e)
+		{
+			base.OnTapped(e);
+
+			txtChildren.Text += "(1/2) Tapped (override)\n";
+		}
+
+		protected override void OnPointerPressed(PointerRoutedEventArgs args)
+		{
+			txtChildren.Text += "PRESSED\n";
+
+			base.OnPointerPressed(args);
+		}
+
+		protected override void OnPointerReleased(PointerRoutedEventArgs args)
+		{
+			txtChildren.Text += "RELEASED\n";
+
+			base.OnPointerReleased(args);
+		}
+	}
+}
